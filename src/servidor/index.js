9d9de92a3fa99487ac127deca3ea778494163a31
Ewerton Souto <otavio.souto@gmail.com>
2018-01-23 20:24:50 -0300

    #12 - corrigindo porta de 3000 para 4000 no arquivo index.js

--- src/servidor/index.js
+++ src/servidor/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const routes = require('./routes');
 
 const root = './';
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '4000';
 const app = express();
 
 app.use(bodyParser.json());
